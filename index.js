a51c5296e2eac09af0ab0d52aeb4844f60ee6fcd
harikrishnan dhanasekaran <harikrishnan.dhanasekaran@zohocorp.com>
2021-06-04 16:29:08 +0530

    bulk screen shot

--- index.js
+++ index.js
@@ -1,31 +1,60 @@
 const puppeteer = require('puppeteer');
 const path = require("path");
 const fs = require('fs');
-var basefolder= "./screenshots";
-// const fullPageScreenshot = require("puppeteer-full-page-screenshot");
-
-async function run(ar) {
-    var randomeName = "folder_"+Math.floor(Math.random()*10000000);
-    
-    ar.forEach((url)=>{
-        var FileName = path.join(basefolder,randomeName,url.replace(/\//ig,"_"));
-        fs.mkdirSync(basefolder+"/"+randomeName,{recursive:true});
-        getScreenShot(url,FileName,randomeName);
-    })
-  
-}
-
-async function getScreenShot(url,FileName,directory){
-    try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(url,{timeout: 0});
-        await page.setViewport({ width: 480, height: 853 });
-        await page.screenshot({ path: FileName.replace(".","_").replace(":","")+".png",fullPage:true});
-        await browser.close();
+var basefolder = path.join(__dirname, "./public/screenshots");
+const express = require('express');
+var file = require('adm-zip');
+var zip = new file();
+const app = express();
+app.use(express.static("./public/"))
+app.use(express.urlencoded({ extended: true }));
+require('events').EventEmitter.prototype._maxListeners = 100;
+//get page
+app.get("/", async (req, res) => { req.send("./public/index.html"); });
+//post requrest
+app.post("/", async (req, res) => {
+    var randomeName = "folder_" + Math.floor(Math.random() * 10000000);
+    var list = req.body.weblinks.split("\r\n");
+    var width = req.body.width;
+    var height = req.body.height;
+    console.log(width);
+    if (list[0] != "" && width!=undefined) {
+        await Promise.all(
+            list.map(
+                async (url) => {
+                    var FileName = await path.join(basefolder, randomeName, url.replace(/\//ig, "_"));
+                    await fs.mkdirSync(basefolder + "/" + randomeName, { recursive: true });
+                    await getScreenShot(url, FileName, randomeName, width, height);
+                }
+            )
+        );
         
-    } catch (error) {
-        console.log(error)
+        await zip.addLocalFolder(path.join(basefolder, randomeName));
+        await zip.writeZip(path.join(basefolder, randomeName + ".zip"));
+        fs.rmdirSync(path.join(basefolder, randomeName), { recursive: true });
+        res.redirect("http://localhost:3290/" + path.join("screenshots", randomeName + ".zip"));
     }
+    else {
+        res.end(JSON.stringify({ error: "invalid links" }))
+    }
+})
+
+app.listen(3290, () => {
+    console.log(`The server running on port number ${3290}`);
+})
+async function getScreenShot(url, FileName, directory, width, height) {
+    await Promise.all(width.map(async (w, index) => {
+        try {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(url, { timeout: 0 });
+            await page.setViewport({ width: parseInt(w), height: parseInt(height[index]) });
+            await page.screenshot({ path: FileName.replace(".", "_").replace(":", "") + w + "x" + height[index] + ".png", fullPage: true });
+            await browser.close();
+            console.log("iteration");
+
+        } catch (error) {
+            console.log(error)
+        }
+    }))
 }
-run(["https://www.google.com","https://www.yahoo.com"]);
